1cd003003ece7b5c78437a7da535ca31091acf0a
mccrush <mccrush2027@gmail.com>
2022-02-08 19:22:04 +0700

    init proj

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+let app
+
+if (!app) {
+  app = createApp(App).mount('#app')
+}
